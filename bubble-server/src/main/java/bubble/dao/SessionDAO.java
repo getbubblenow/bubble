feef9af6d322cd8065349b036ab75750590e8bbd
Jonathan Cobb <jonathan@kyuss.org>
2020-01-13 23:20:28 -0500

    whitespace nit

--- bubble-server/src/main/java/bubble/dao/SessionDAO.java
+++ bubble-server/src/main/java/bubble/dao/SessionDAO.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class SessionDAO extends AbstractSessionDAO<Account> {
 
-    @Override protected boolean canStartSession(Account account) {
-        return !account.suspended();
-    }
+    @Override protected boolean canStartSession(Account account) { return !account.suspended(); }
 
 }
