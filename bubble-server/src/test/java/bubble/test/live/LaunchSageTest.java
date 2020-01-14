f2b772d66bfd23c8c6f43729b3e1dd7fa2caca08
Jonathan Cobb <jonathan@kyuss.org>
2020-01-14 17:30:20 -0500

    fixes to launch sage test

--- bubble-server/src/test/java/bubble/test/live/LaunchSageTest.java
+++ bubble-server/src/test/java/bubble/test/live/LaunchSageTest.java
@@ -1,14 +1,21 @@
 package bubble.test.live;
 
+import bubble.cloud.CloudServiceDriver;
+import bubble.cloud.dns.godaddy.GoDaddyDnsDriver;
 import org.junit.Test;
+
+import static bubble.ApiConstants.getBubbleDefaultDomain;
 
 public class LaunchSageTest extends LiveTestBase {
 
-    @Override protected String getTestSageFqdn() { return null; }
-    @Override protected boolean shouldStopSage() { return false; }
-
     @Override protected String getManifest() { return "manifest-live"; }
 
-    @Test public void testLaunchSage () throws Exception {}
+    @Override protected Class<? extends CloudServiceDriver> getPublicDnsDriver() { return GoDaddyDnsDriver.class; }
+
+    @Override protected String getTestSageFqdn() { return null; }
+    @Override protected boolean shouldStopSage() { return false; }
+    @Override public String getDefaultDomain() { return getBubbleDefaultDomain(); }
+
+    @Test public void testLaunchSage () {}
 
 }
