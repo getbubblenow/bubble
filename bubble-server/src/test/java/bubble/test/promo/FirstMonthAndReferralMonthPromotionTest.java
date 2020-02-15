f48eda5ac3083604cca44e0e34d6fc5d1bfb9d96
Jonathan Cobb <jonathan@kyuss.org>
2020-02-15 17:40:17 -0500

    fix manifest for test

--- bubble-server/src/test/java/bubble/test/promo/FirstMonthAndReferralMonthPromotionTest.java
+++ bubble-server/src/test/java/bubble/test/promo/FirstMonthAndReferralMonthPromotionTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 @Slf4j
 public class FirstMonthAndReferralMonthPromotionTest extends PaymentTestBase {
 
-    @Override protected String getManifest() { return "promo/1mo_and_referral/manifest_1m_and_referral"; }
+    @Override protected String getManifest() { return "promo/1mo_and_referral/manifest_1mo_and_referral"; }
 
     @Test public void testFirstMonthAndMultipleReferralMonthsFree () throws Exception {
         modelTest("promo/first_month_and_multiple_referral_months_free");
