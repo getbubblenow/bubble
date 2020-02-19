5746143180d22fe9dea50a4d3362b8b760bff4fe
Jonathan Cobb <jonathan@kyuss.org>
2020-02-19 11:17:49 -0500

    run certbot if either cert is missing

--- automation/roles/nginx/files/init_certbot.sh
+++ automation/roles/nginx/files/init_certbot.sh
@@ -7,7 +7,7 @@ if [[ $(find /etc/letsencrypt/accounts -type f -name regr.json | xargs grep -l \
   certbot register --agree-tos -m "${LE_EMAIL}" --non-interactive
 fi
 
-if [[ ! -f /etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem ]] ; then
+if [[ ! -f /etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem || ! -f /etc/letsencrypt/live/${SERVER_ALIAS}/fullchain.pem ]] ; then
   certbot certonly --standalone --non-interactive -d ${SERVER_NAME}
   certbot certonly --standalone --non-interactive -d ${SERVER_ALIAS}
 else
