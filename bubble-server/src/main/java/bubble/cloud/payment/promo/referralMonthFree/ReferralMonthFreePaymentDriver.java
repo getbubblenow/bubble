560150abea783eb5987430fcf7041b02fb67ca65
Jonathan Cobb <jonathan@kyuss.org>
2020-02-26 05:21:10 -0500

    add copyright/license comments

--- bubble-server/src/main/java/bubble/cloud/payment/promo/referralMonthFree/ReferralMonthFreePaymentDriver.java
+++ bubble-server/src/main/java/bubble/cloud/payment/promo/referralMonthFree/ReferralMonthFreePaymentDriver.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2020 Bubble, Inc.  All rights reserved.
+ * For personal (non-commercial) use, see license: https://git.bubblev.org/bubblev/bubble/src/branch/master/LICENSE.md
+ */
 package bubble.cloud.payment.promo.referralMonthFree;
 
 import bubble.cloud.payment.promo.PromotionPaymentConfig;
--- bubble-server/src/main/java/bubble/service/account/StandardAccountMessageService.java
+++ bubble-server/src/main/java/bubble/service/account/StandardAccountMessageService.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2020 Bubble, Inc.  All rights reserved.
+ * For personal (non-commercial) use, see license: https://git.bubblev.org/bubblev/bubble/src/branch/master/LICENSE.md
+ */
 package bubble.service.account;
 
 import bubble.cloud.auth.AuthenticationDriver;
