7d8577d24a208d9bda6cf9e259f7d0cd8cf5647c
Jonathan Cobb <jonathan@kyuss.org>
2020-02-28 17:37:57 -0500

    add file header

--- bubble-server/src/main/java/bubble/service/account/AuthenticatorService.java
+++ bubble-server/src/main/java/bubble/service/account/AuthenticatorService.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2020 Bubble, Inc.  All rights reserved.
+ * For personal (non-commercial) use, see license: https://bubblev.com/bubble-license/
+ */
 package bubble.service.account;
 
 import bubble.model.account.Account;
