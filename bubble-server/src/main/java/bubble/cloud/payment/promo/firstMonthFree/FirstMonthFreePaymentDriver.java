20a7075eca4ef030cd0699c2e1cdf5569518058a
Jonathan Cobb <jonathan@kyuss.org>
2020-03-23 17:05:01 -0400

    allow first month free to be applied anytime

--- bubble-server/src/main/java/bubble/cloud/payment/promo/firstMonthFree/FirstMonthFreePaymentDriver.java
+++ bubble-server/src/main/java/bubble/cloud/payment/promo/firstMonthFree/FirstMonthFreePaymentDriver.java
@@ -21,11 +21,11 @@ public class FirstMonthFreePaymentDriver extends PromotionalPaymentDriverBase<Pr
 
     @Override public boolean addPromoToAccount(Promotion promo, Account caller) {
         // caller must not have any bills
-        final int billCount = billDAO.countByAccount(caller.getUuid());
-        if (billCount != 0) {
-            log.warn("applyPromo: promo="+promo.getName()+", account="+caller.getName()+", account must have no Bills, found "+billCount+" bills");
-            return false;
-        }
+//        final int billCount = billDAO.countByAccount(caller.getUuid());
+//        if (billCount != 0) {
+//            log.warn("applyPromo: promo="+promo.getName()+", account="+caller.getName()+", account must have no Bills, found "+billCount+" bills");
+//            return false;
+//        }
         // does the caller already have one of these?
         final List<AccountPaymentMethod> existingCreditPaymentMethods = paymentMethodDAO.findByAccountAndCloud(caller.getUuid(), promo.getCloud());
         if (!empty(existingCreditPaymentMethods)) {
@@ -50,16 +50,16 @@ public class FirstMonthFreePaymentDriver extends PromotionalPaymentDriverBase<Pr
                                        AccountPlan accountPlan,
                                        AccountPaymentMethod paymentMethod) {
         // must have exactly one bill, this bill
-        final List<Bill> bills = billDAO.findByAccount(accountPlan.getAccount());
-        if (bills.size() != 1 || !bills.get(0).getUuid().equals(bill.getUuid())) {
-            return false;
-        }
-
-        // must not have used this promotion before
-        final List<AccountPaymentMethod> existingFirstMonthFree = paymentMethodDAO.findByAccountAndCloud(accountPlan.getAccount(), promo.getCloud());
-        if (existingFirstMonthFree.size() != 1 || !existingFirstMonthFree.get(0).getPromotion().equals(promo.getUuid())) {
-            return false;
-        }
+//        final List<Bill> bills = billDAO.findByAccount(accountPlan.getAccount());
+//        if (bills.size() != 1 || !bills.get(0).getUuid().equals(bill.getUuid())) {
+//            return false;
+//        }
+//
+//        // must not have used this promotion before
+//        final List<AccountPaymentMethod> existingFirstMonthFree = paymentMethodDAO.findByAccountAndCloud(accountPlan.getAccount(), promo.getCloud());
+//        if (existingFirstMonthFree.size() != 1 || !existingFirstMonthFree.get(0).getPromotion().equals(promo.getUuid())) {
+//            return false;
+//        }
 
         return super.canUseNow(bill, promo, promoDriver, promos, usable, accountPlan, paymentMethod);
     }
