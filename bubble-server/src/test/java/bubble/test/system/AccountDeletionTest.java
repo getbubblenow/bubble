2cbc4bd9305d54c6b4e61b7f559c55e7f71f8441
Kristijan Mitrovic <kmitrovic@itekako.com>
2020-04-15 09:46:02 +0200

    Test with assert if policy object is removed

--- bubble-server/src/test/java/bubble/test/system/AccountDeletionTest.java
+++ bubble-server/src/test/java/bubble/test/system/AccountDeletionTest.java
@@ -5,12 +5,16 @@
 package bubble.test.system;
 
 import bubble.dao.account.AccountDAO;
+import bubble.dao.account.AccountPolicyDAO;
 import bubble.model.account.Account;
 import bubble.test.ActivatedBubbleModelTestBase;
 import lombok.extern.slf4j.Slf4j;
 import org.cobbzilla.wizard.model.HashedPassword;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 @Slf4j
 public class AccountDeletionTest extends ActivatedBubbleModelTestBase {
@@ -25,5 +29,16 @@ public class AccountDeletionTest extends ActivatedBubbleModelTestBase {
     }
 
     @Test public void testFullAccountDeletion() throws Exception { modelTest("account_deletion/full_delete_account"); }
+    @Test public void testBlockAccountDeletion() throws Exception {
+        modelTest("account_deletion/block_delete_account");
+
+        final var accPolicyDAO = getConfiguration().getBean(AccountPolicyDAO.class);
+        final var policies = accPolicyDAO.findAll();
+        assertEquals("Not all objects removed for deleted account", 1, policies.size());
+
+        final var accDAO = getConfiguration().getBean(AccountDAO.class);
+        final var acc = accDAO.findById(policies.get(0).getAccount());
+        assertEquals("The one policy should be for the only existing account", "root", acc.getName());
+    }
 
 }
