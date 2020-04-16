9f9d2693f63fbfaba2278415f1760794dacdb9af
Kristijan Mitrovic <kmitrovic@itekako.com>
2020-04-16 14:45:47 +0200

    Add initial test for keeping payment info of delete account

--- bubble-server/src/test/java/bubble/test/system/AccountDeletionTest.java
+++ bubble-server/src/test/java/bubble/test/system/AccountDeletionTest.java
@@ -5,10 +5,8 @@
 package bubble.test.system;
 
 import bubble.test.ActivatedBubbleModelTestBase;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-@Slf4j
 public class AccountDeletionTest extends ActivatedBubbleModelTestBase {
 
     @Override protected String getManifest() { return "manifest-test"; }
@@ -16,4 +14,9 @@ public class AccountDeletionTest extends ActivatedBubbleModelTestBase {
     @Test public void testFullAccountDeletion() throws Exception { modelTest("account_deletion/full_delete_account"); }
     @Test public void testBlockAccountDeletion() throws Exception { modelTest("account_deletion/block_delete_account"); }
 
+    @Test public void testDeleteAccountWithPayments() throws Exception {
+        modelTest("account_deletion/delete_account_with_payments");
+
+        // TODO: Check if all payment data is archived for the deleted user
+    }
 }
