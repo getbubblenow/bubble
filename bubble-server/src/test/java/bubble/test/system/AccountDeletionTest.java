9857b042f7248fa9bf8929d954b158528089e942
Kristijan Mitrovic <kmitrovic@itekako.com>
2020-04-16 14:25:54 +0200

    Remove not needed before method in test

--- bubble-server/src/test/java/bubble/test/system/AccountDeletionTest.java
+++ bubble-server/src/test/java/bubble/test/system/AccountDeletionTest.java
@@ -4,25 +4,14 @@
  */
 package bubble.test.system;
 
-import bubble.dao.account.AccountDAO;
-import bubble.model.account.Account;
 import bubble.test.ActivatedBubbleModelTestBase;
 import lombok.extern.slf4j.Slf4j;
-import org.cobbzilla.wizard.model.HashedPassword;
-import org.junit.Before;
 import org.junit.Test;
 
 @Slf4j
 public class AccountDeletionTest extends ActivatedBubbleModelTestBase {
 
     @Override protected String getManifest() { return "manifest-test"; }
-
-    @Before public void resetRootPassword() {
-        // reset root password, some tests change it
-        final AccountDAO accountDAO = getConfiguration().getBean(AccountDAO.class);
-        final Account rootUser = accountDAO.findFirstAdmin();
-        accountDAO.update(rootUser.setHashedPassword(new HashedPassword(ROOT_PASSWORD)));
-    }
 
     @Test public void testFullAccountDeletion() throws Exception { modelTest("account_deletion/full_delete_account"); }
     @Test public void testBlockAccountDeletion() throws Exception { modelTest("account_deletion/block_delete_account"); }
