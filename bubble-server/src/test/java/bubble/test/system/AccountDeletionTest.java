a206eafb55a074c61c5adb812c6c3322ffd00d37
Kristijan Mitrovic <kmitrovic@itekako.com>
2020-04-22 16:01:33 +0200

    Add test for arhiciving payments on full account deletion

--- bubble-server/src/test/java/bubble/test/system/AccountDeletionTest.java
+++ bubble-server/src/test/java/bubble/test/system/AccountDeletionTest.java
@@ -5,12 +5,17 @@
 package bubble.test.system;
 
 import bubble.dao.account.AccountDAO;
+import bubble.dao.bill.AccountPaymentArchivedDAO;
 import bubble.model.account.Account;
 import bubble.model.bill.AccountPayment;
 import bubble.model.bill.AccountPaymentMethod;
 import bubble.model.bill.Bill;
 import bubble.test.ActivatedBubbleModelTestBase;
+import lombok.NonNull;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -19,16 +24,25 @@ public class AccountDeletionTest extends ActivatedBubbleModelTestBase {
 
     @Override protected String getManifest() { return "manifest-test"; }
 
+    @Before public void truncatePaymentArchive() {
+        final var archivedInfoDAO = getBean(AccountPaymentArchivedDAO.class);
+        archivedInfoDAO.delete(archivedInfoDAO.findAll());
+    }
+
     @Test public void testFullAccountDeletion() throws Exception { modelTest("account_deletion/full_delete_account"); }
     @Test public void testBlockAccountDeletion() throws Exception { modelTest("account_deletion/block_delete_account"); }
 
-    @Test public void testDeleteAccountWithPayments() throws Exception {
-        final var archivedInfoDAO = getBean(bubble.dao.bill.AccountPaymentArchivedDAO.class);
-        assertEquals("Starting database contains some archived payments", 0, archivedInfoDAO.countAll().intValue());
+    @Test public void testBlockDeleteAccountWithPayments() throws Exception {
+        checkArchivedPayments(modelTest("account_deletion/block_delete_account_with_payments"));
+    }
 
-        final var modelTestCtx = modelTest("account_deletion/delete_account_with_payments");
+    @Test public void testFullDeleteAccountWithPayments() throws Exception {
+        checkArchivedPayments(modelTest("account_deletion/full_delete_account_with_payments"));
+    }
 
+    private void checkArchivedPayments(@NonNull final Map<String, Object> modelTestCtx) {
         final var accountDAO = getBean(AccountDAO.class);
+        final var archivedInfoDAO = getBean(AccountPaymentArchivedDAO.class);
 
         final var deletedAccounts = accountDAO.findDeleted();
         // the account was fully deleted at the end of the JSON test
