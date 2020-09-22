ffcb46ef29173fc63a847894549bebaab8975380
Jonathan Cobb <jonathan@kyuss.org>
2020-09-22 07:04:36 -0400

    use fqdn for NodeProgressMeter thread

--- bubble-server/src/main/java/bubble/service/cloud/NodeProgressMeter.java
+++ bubble-server/src/main/java/bubble/service/cloud/NodeProgressMeter.java
@@ -94,7 +94,7 @@ public class NodeProgressMeter extends PipedOutputStream implements Runnable {
         writer = new BufferedWriter(new OutputStreamWriter(this));
 
         thread = new Thread(this);
-        thread.setName("NodeProgressMeter-"+nn.getNetworkName());
+        thread.setName("NodeProgressMeter-"+nn.getFqdn());
         thread.setDaemon(true);
         thread.start();
     }
