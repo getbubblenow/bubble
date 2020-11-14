8f2c650baca9090fb4f4abbe42123a4d9f0ad7ae
Jonathan Cobb <jonathan@kyuss.org>
2020-11-13 22:29:53 -0500

    build image before checking version, fix test

--- bubble-server/src/test/java/bubble/test/promo/MultiplePromotionsTest.java
+++ bubble-server/src/test/java/bubble/test/promo/MultiplePromotionsTest.java
@@ -4,14 +4,18 @@
  */
 package bubble.test.promo;
 
+import bubble.auth.PromoCodePolicy;
 import bubble.test.payment.PaymentTestBase;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 
 @Slf4j
 public class MultiplePromotionsTest extends PaymentTestBase {
 
     @Override protected String getManifest() { return "promo/multi/manifest_multi"; }
+
+    @Before public void ensurePromoCodeOptional() { getConfiguration().setPromoCodePolicy(PromoCodePolicy.optional); }
 
     @Test public void testMultiplePromotions() throws Exception { modelTest("promo/multi_promo"); }
 
