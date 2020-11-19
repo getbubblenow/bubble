faf51f24825c8860e38db3d30042ead3267e41a0
Jonathan Cobb <jonathan@kyuss.org>
2020-11-19 15:24:53 -0500

    make packer installation more robust

--- bin/install_packer.sh
+++ bin/install_packer.sh
@@ -7,18 +7,21 @@ function die {
   exit 1
 }
 
+PLATFORM="$(uname -s)"
+
 # Install packer
 if [[ ! -f ${HOME}/packer/packer ]] ; then
   PACKER_VERSION=1.6.5
-  if [[ "$(uname)" == "Darwin" ]] ; then
+  if [[ "${PLATFORM}" == "Darwin" ]] ; then
     PACKER_FILE=packer_${PACKER_VERSION}_darwin_amd64.zip
-  elif [[ "$(uname)" == "Linux" ]] ; then
+  elif [[ "${PLATFORM}" == "Linux" ]] ; then
     PACKER_FILE=packer_${PACKER_VERSION}_linux_amd64.zip
   else
-    die "Add packer support to script ${0} for uname $(uname)"
+    die "Add packer support to script ${0} for platform ${PLATFORM}"
   fi
   PACKER_URL=https://releases.hashicorp.com/packer/${PACKER_VERSION}/${PACKER_FILE}
   mkdir -p ${HOME}/packer && cd ${HOME}/packer && curl -L ${PACKER_URL} -o ${PACKER_FILE} && unzip ${PACKER_FILE} || die "Error installing packer"
+  echo "Packer successfully installed"
 else
   echo "Packer already installed"
 fi
@@ -26,15 +29,16 @@ fi
 # Install packer Vultr plugin
 if [[ ! -f ${HOME}/.packer.d/plugins/packer-builder-vultr ]] ; then
   PACKER_VULTR_VERSION=1.0.15
-  if [[ "$(uname)" == "Darwin" ]] ; then
+  if [[ "${PLATFORM}" == "Darwin" ]] ; then
     PACKER_VULTR_FILE=packer-builder-vultr_${PACKER_VULTR_VERSION}_macOs_64-bit.tar.gz
-  elif [[ "$(uname)" == "Linux" ]] ; then
+  elif [[ "${PLATFORM}" == "Linux" ]] ; then
     PACKER_VULTR_FILE=packer-builder-vultr_${PACKER_VULTR_VERSION}_linux_64-bit.tar.gz
   else
-    die "Add packer vultr support to script ${0} for uname $(uname)"
+    die "Add packer vultr support to script ${0} for platform ${PLATFORM}"
   fi
   PACKER_VULTR_URL=https://github.com/vultr/packer-builder-vultr/releases/download/v${PACKER_VULTR_VERSION}/${PACKER_VULTR_FILE}
   mkdir -p ${HOME}/.packer.d/plugins && cd ${HOME}/.packer.d/plugins && curl -L ${PACKER_VULTR_URL} -o ${PACKER_VULTR_FILE} && tar xzf ${PACKER_VULTR_FILE}  || die "Error installing packer vultr plugin"
+  echo "Packer Vult plugin successfully installed"
 else
   echo "Packer vultr plugin already installed"
 fi
