c1c365b5c8554c4213a93401f0898aa0186e0fe9
Jonathan Cobb <jonathan@kyuss.org>
2020-11-21 21:35:25 -0500

    use marker file to determine if gitmodules file should be updated

--- bin/git_https_submodules.sh
+++ bin/git_https_submodules.sh
@@ -7,12 +7,21 @@
 SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
 GIT_MODS="${SCRIPT_DIR}/../.gitmodules"
 GIT_TEMP_FILE="$(mktemp "${GIT_MODS}.XXXXXXX")"
-cat "${GIT_MODS}" | while read -r line ; do
-  if [[ $(echo "${line}" | grep -c 'url = ') -gt 0 && $(echo "${line}" | grep -c 'git@git') -gt 0 ]] ; then
+CHANGED_MARKER="${GIT_TEMP_FILE}.use"
+cat "${GIT_MODS}" | while read -r line; do
+  if [[ $(echo "${line}" | grep -c 'url = ') -gt 0 && $(echo "${line}" | grep -c 'git@git') -gt 0 ]]; then
     REPO="$(echo "${line}" | awk -F ':' '{print $2}')"
     echo "url = https://git.bubblev.org/${REPO}" | tee -a "${GIT_TEMP_FILE}"
+    touch "${CHANGED_MARKER}"
   else
     echo "${line}" | tee -a "${GIT_TEMP_FILE}"
   fi
 done
-mv "${GIT_TEMP_FILE}" "${GIT_MODS}"
+
+if [[ -f "${CHANGED_MARKER}" ]]; then
+  cd "${SCRIPT_DIR}/.." &&
+    git update-index --assume-unchanged "$(basename "${GIT_MODS}")" &&
+    mv "${GIT_TEMP_FILE}" "${GIT_MODS}" ||
+    echo "$0: error updating file: ${GIT_MODS}"
+fi
+rm -f "${CHANGED_MARKER}"
